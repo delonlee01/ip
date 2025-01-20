1d49fde019fea9530ad4cb5e46fc866da0a2cb18
delonlee01 <delonlee2001@hotmail.com>
2025-01-20 20:27:42 +0800

    Add user input support

--- src/main/java/Woody.java
+++ src/main/java/Woody.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Woody {
     public static void main(String[] args) {
         String logo = """
@@ -11,6 +13,17 @@ public class Woody {
         System.out.print(logo);
         System.out.println("Hello! I'm Woody");
         System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String userCmd = sc.nextLine();
+            if (userCmd.equals("bye")) {
+                break;
+            }
+            System.out.println(userCmd);
+        }
+
         System.out.println("Bye! Hope to see you again soon!");
+        sc.close();
     }
 }
