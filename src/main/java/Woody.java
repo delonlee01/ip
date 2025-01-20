d1ec55780e08d7ed2c69bdee14e29a6fd05cbac7
delonlee01 <delonlee2001@hotmail.com>
2025-01-20 20:46:57 +0800

    Add list command

--- src/main/java/Woody.java
+++ src/main/java/Woody.java
@@ -15,12 +15,24 @@ public class Woody {
         System.out.println("What can I do for you?");
 
         Scanner sc = new Scanner(System.in);
+        String[] history = new String[100];
+        int idx = 0;
         while (true) {
             String userCmd = sc.nextLine();
             if (userCmd.equals("bye")) {
                 break;
             }
-            System.out.println(userCmd);
+            if (userCmd.equals("list")) {
+                for (int i = 0; i < history.length; i++) {
+                    String item = history[i];
+                    if (item != null) {
+                        System.out.println(String.format("%d. %s", i + 1, history[i]));
+                    }
+                }
+            } else {
+                history[idx++] = userCmd;
+                System.out.println("added: " + userCmd);
+            }
         }
 
         System.out.println("Bye! See you soon partner!");
