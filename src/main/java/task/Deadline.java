0f012590c3bd92365ec36f312eba394239f7ab50
delonlee01 <delonlee2001@hotmail.com>
2025-01-28 21:34:36 +0800

    Modify tasks to store date strings as LocalDate

--- src/main/java/task/Deadline.java
+++ src/main/java/task/Deadline.java
@@ -1,9 +1,13 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import exception.InvalidArgumentsException;
 
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDate by;
 
     public Deadline(String args) throws InvalidArgumentsException {
         String[] tokens = args.split(" /by ");
@@ -11,17 +15,23 @@ public class Deadline extends Task {
             throw new InvalidArgumentsException("\"deadline\" requires a description, and by.");
         }
         this.description = tokens[0].strip();
-        this.by = tokens[1].strip();
+        try {
+            this.by = LocalDate.parse(tokens[1].strip(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidArgumentsException("\"deadline\" requires by in the format: dd/MM/yyyy.");
+        }
     }
 
     @Override
     public String toDataString() {
         int status = this.isDone ? 1 : 0;
-        return String.format("D|%d|%s /by %s", status, this.description, this.by);
+        return String.format("D|%d|%s /by %s", status, this.description,
+                this.by.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), this.by);
+        return String.format("[D]%s (by: %s)", super.toString(),
+                this.by.format(DateTimeFormatter.ofPattern("dd MMM yyyy")));
     }
 }
--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -1,10 +1,14 @@
 package task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 import exception.InvalidArgumentsException;
 
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String args) throws InvalidArgumentsException {
         String[] tokens = args.split("(/from|/to)");
@@ -12,18 +16,26 @@ public class Event extends Task {
             throw new InvalidArgumentsException("\"event\" requires a description, from, and to.");
         }
         this.description = tokens[0].strip();
-        this.from = tokens[1].strip();
-        this.to = tokens[2].strip();
+        try {
+            this.from = LocalDate.parse(tokens[1].strip(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            this.to = LocalDate.parse(tokens[2].strip(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new InvalidArgumentsException("\"event\" requires from and to in the format: dd/MM/yyyy.");
+        }
     }
 
     @Override
     public String toDataString() {
         int status = this.isDone ? 1 : 0;
-        return String.format("E|%d|%s /from %s /to %s", status, this.description, this.from, this.to);
+        return String.format("E|%d|%s /from %s /to %s", status, this.description,
+                this.from.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+                this.to.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(),
+                this.from.format(DateTimeFormatter.ofPattern("dd MMM yyyy")),
+                this.to.format(DateTimeFormatter.ofPattern("dd MMM yyyy")));
     }
 }
