4ff5b74dc09f48222560eb5df067e47b24a045a0
delonlee01 <delonlee2001@hotmail.com>
2025-02-05 23:28:01 +0800

    Add the basic GUI

--- src/main/java/woody/Woody.java
+++ src/main/java/woody/Woody.java
@@ -46,4 +46,11 @@ public class Woody {
             }
         }
     }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Woody heard: " + input;
+    }
 }
